d0e375fccdbc6264082a3f55371b11ba9cdcf42e
Carolina Cedeño Loor <caroldev@outlook.es>
2024-03-22 01:13:43 +0100

    non_functions

---
+++ cursus/libft/ft_putnbr_fd.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccedeno- <ccedeno-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/22 00:47:27 by ccedeno-          #+#    #+#             */
+/*   Updated: 2024/03/22 01:07:41 by ccedeno-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_putnbr_fd(int n, int fd)
+{
+	unsigned int    nbr;
+
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nbr = n * -1;
+	}
+	else
+		nbr = n;
+	if (nbr / 10 != 0)
+		ft_putnbr_fd(nbr / 10, fd);
+	ft_putchar_fd(nbr % 10 + 48, fd);
+}
