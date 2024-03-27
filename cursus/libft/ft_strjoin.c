122637cbfe362706da93cc4d51f5495a708d79b4
Carolina Cedeño Loor <147629979+cedelin@users.noreply.github.com>
2024-03-27 15:00:38 +0000

    ft_strjoin sin comprobar paco

---
+++ cursus/libft/ft_strjoin.c
@@ -0,0 +1,26 @@
+#include "libft.h"
+
+char *ft_strjoin(const char str1, const char str2)
+{
+	char	*result;
+	size_t	n;
+	size_t	i;
+
+	result = (char *)malloc(ft_strlen(str1) + ft_strlen(str2) + 1);
+	if (result = NULL)
+		return (NULL);
+	i = 0;
+	while (str1[i] != '\0')
+	{
+		result[i] = str1[i];
+		i++;
+	}
+	n = 0;
+	while (str2[n] != '\0')
+	{
+		result[i + n] = str2[n];
+		n++;
+	}
+	result[i + j] = '\0';
+	return (result);
+}
