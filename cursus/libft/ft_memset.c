8790b48f738be95a139707236d5ad14284480746
Carolina Cedeño Loor <147629979+cedelin@users.noreply.github.com>
2024-04-01 13:26:27 +0200

    memset

--- cursus/libft/ft_memset.c
+++ cursus/libft/ft_memset.c
@@ -6,23 +6,23 @@
 /*   By: ccedeno- <ccedeno-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 14:28:23 by ccedeno-          #+#    #+#             */
-/*   Updated: 2024/03/26 12:30:46 by ccedeno-         ###   ########.fr       */
+/*   Updated: 2024/03/26 14:03:15 by ccedeno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *str, int c, size_t n)
+void	*ft_memset(void *s, int c, size_t n)
 {
 	char *ptr;
 	unsigned char x;
 	
-	ptr = (char *) str;
+	ptr = (char *) s;
 	x = (unsigned char) c;
 	while (n > 0)
 	{
 		ptr[n -1] = x;
 		n--;
 	}
-	return (str);
+	return (s);
 }
--- cursus/libft/libft.h
+++ cursus/libft/libft.h
@@ -13,7 +13,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int	    ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void * src, size_t n);
 void	ft_memmove(void *dest, const void *src, size_t len);
-void	*ft_memset(void *str, int c, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
 char    *ft_strchr(const char *s, int c);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
