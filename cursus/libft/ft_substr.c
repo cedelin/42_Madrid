cf2372827a2a2d3c548571f26be0cb37acdca67d
Carolina C. Loor <caroldev@outlook.es>
2024-04-01 14:12:30 +0200

    substr

---
+++ cursus/libft/ft_substr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccedeno- <ccedeno-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/01 13:27:57 by ccedeno-          #+#    #+#             */
+/*   Updated: 2024/04/01 13:27:57 by ccedeno-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char *substr;
+	size_t	i;
+	size_t	x;
+
+	substr = (char *)malloc(len + 1);
+	if (substr == 0)
+		return (NULL);
+	i = 0;
+	x = 0;
+	while(s[i])
+	{
+		if (i >= start && j < len)
+		{
+			substr[x] = s[i];
+			x++;
+		}
+		i++;
+	}
+	substr[j] = 0;
+	return (substr);
+}
